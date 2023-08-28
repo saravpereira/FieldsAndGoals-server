c4c7a5c7a22deed9746cd1b054510804a72793e7
Nathan Pham <nghiapham102603@gmail.com>
2023-08-27 22:18:50 -0700

    Added scraping functionality for headlines from espn.com

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const bodyParser = require("body-parser");
 const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+const cheerio = require("cheerio");
 require("dotenv").config();
+
 const app = express();
 const mongoose = require("mongoose");
 const todoRoutes = require("./routes/todo.js");
@@ -21,6 +24,27 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/todo", todoRoutes);
+
+// Web scraping endpoint
+app.get("/scrape-espn", async (req, res) => {
+  try {
+    console.log("Request received for /scrape-espn");
+    const response = await axios.get("https://www.espn.com");
+    const $ = cheerio.load(response.data);
+
+    const headlines = [];
+
+    $('.headlineStack__list li').each((index, element) => {
+      const headline = $(element).text().trim();
+      headlines.push(headline);
+    });
+
+    res.json({ headlines });
+  } catch (error) {
+    console.error("Error scraping:", error);
+    res.status(500).json({ error: "Error scraping data" });
+  }
+});
 
 app.listen(8080, () => {
   console.log("listening on port 8080");
