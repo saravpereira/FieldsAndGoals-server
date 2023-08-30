d91fbcfa383006badf6364f9f09cb3b3ca88f303
Nathan Pham <87776690+nghiapham1026@users.noreply.github.com>
2023-08-30 02:57:33 +0000

    Scraped sofascore.com

--- controllers/webscrapeController.js
+++ controllers/webscrapeController.js
@@ -1,30 +1,33 @@
 const axios = require("axios");
-const { scoresURL } = require("./constants")
+const cheerio = require("cheerio");
+const puppeteer = require("puppeteer");
+const { sofascoreURL } = require("./constants");
 
 async function scrapeEspn(req, res) {
   try {
-    const config = {
-      method: 'get',
-      maxBodyLength: Infinity,
-      url: scoresURL,
-      headers: { 
-        'authority': 'site.web.api.espn.com', 
-        'accept': '*/*', 
-        'accept-language': 'en-US,en;q=0.9,vi-VN;q=0.8,vi;q=0.7,es-US;q=0.6,es;q=0.5', 
-        'origin': 'https://www.espn.com', 
-        'referer': 'https://www.espn.com/', 
-        'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"', 
-        'sec-ch-ua-mobile': '?0', 
-        'sec-ch-ua-platform': '"Windows"', 
-        'sec-fetch-dest': 'empty', 
-        'sec-fetch-mode': 'cors', 
-        'sec-fetch-site': 'same-site', 
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
-      }
-    };
+    console.log("Request received");
+    
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(sofascoreURL);
 
-    const response = await axios.request(config);
-    res.json(response.data); // Respond with the entire data from the API response
+    await page.waitForSelector(".sc-fqkvVR.sc-dcJsrY.ditMfZ.hNspbb.sc-2cf4ebd1-0.cLVSQH.js-list-cell-target.js-list-cell-target-11542832");
+
+    const matches = await page.evaluate(() => {
+      const matchElements = document.querySelectorAll(".sc-fqkvVR.sc-dcJsrY.ditMfZ.hNspbb.sc-2cf4ebd1-0.cLVSQH.js-list-cell-target.js-list-cell-target-11542832");
+      const matchData = [];
+
+      matchElements.forEach((element) => {
+        const matchText = element.innerText.trim();
+        matchData.push(matchText);
+      });
+
+      return matchData;
+    });
+
+    await browser.close();
+
+    res.json({ matches });
   } catch (error) {
     console.error("Error scraping:", error);
     res.status(500).json({ error: "Error scraping data" });
