62c6f8655d763aef8b12676b1659f45b35ec795d
Nathan Pham <87776690+nghiapham1026@users.noreply.github.com>
2023-08-31 17:19:24 +0000

    Switched scraper to Puppeteer

--- controllers/webscrapeController.js
+++ controllers/webscrapeController.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const cheerio = require("cheerio");
+const puppeteer = require("puppeteer");
 const { scoresURL } = require("./constants");
 
 const MIN_DELAY = 1000;
@@ -28,7 +28,7 @@ async function processRequestQueue() {
     try {
       const response = await axios.get(url, {
         headers: {
-          'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 7_4_0; like Mac OS X) AppleWebKit/534.49 (KHTML, like Gecko)  Chrome/53.0.1780.199 Mobile Safari/533.3'
+          'User-Agent': 'Mozilla/5.0 (compatible; MSIE 7.0; Windows; U; Windows NT 10.0; x64 Trident/4.0)'
         }
       });
 
@@ -47,46 +47,59 @@ async function scrapeEspn(req, res) {
     const date = "20230827";
     const url = `${scoresURL}/_/date/${date}`;
 
-    const requestPromise = new Promise(resolve => {
-      requestQueue.push({ url, resolve });
-    });
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-    await processRequestQueue();
+    // Set a user agent for the page
+    await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 7_4_0; like Mac OS X) AppleWebKit/534.49 (KHTML, like Gecko)  Chrome/53.0.1780.199 Mobile Safari/533.3');
 
-    const response = await requestPromise;
+    await page.goto(url);
+    await page.waitForSelector(".Scoreboard__RowContainer");
 
-    if (response === null) {
-      res.status(500).json({ error: "Error scraping data" });
-      return;
-    }
+    const matchData = await page.evaluate(() => {
+      const matchData = [];
+      const date = "20230827";
 
-    const html = response.data;
-    const $ = cheerio.load(html);
+      const matchElements = document.querySelectorAll(".Scoreboard__RowContainer");
 
-    const matchData = [];
+      matchElements.forEach((container) => {
+        const matchInfo = {};
     
-    $(".Scoreboard__RowContainer").each((index, container) => {
-      const matchInfo = {};
-
-      const matchDateArray = date.match(/(\d{4})(\d{2})(\d{2})/);
-      const formattedMatchDate = `${matchDateArray[1]}-${matchDateArray[2]}-${matchDateArray[3]}`;
-      matchInfo.matchDate = formattedMatchDate;
-
-      matchInfo.league = $(container).closest(".Card").find(".Card__Header__Title").text().trim();
-      matchInfo.homeTeam = $(container).find(".ScoreboardScoreCell__Item--home .ScoreCell__TeamName").text().trim();
-      matchInfo.awayTeam = $(container).find(".ScoreboardScoreCell__Item--away .ScoreCell__TeamName").text().trim();
-      matchInfo.homeScore = $(container).find(".ScoreboardScoreCell__Item--home .ScoreCell__Score").text().trim();
-      matchInfo.awayScore = $(container).find(".ScoreboardScoreCell__Item--away .ScoreCell__Score").text().trim();
-      matchInfo.matchStatus = $(container).find(".ScoreCell__Time").text().trim(); 
-
-      matchInfo.homeLogo = $(container).find(".ScoreboardScoreCell__Item--home .ScoreboardScoreCell__Logo").attr("src");
-      matchInfo.awayLogo = $(container).find(".ScoreboardScoreCell__Item--away .ScoreboardScoreCell__Logo").attr("src");
-
-      matchData.push(matchInfo);
+        matchInfo.matchDate = date;
+    
+        const leagueElement = container.closest(".Card")?.querySelector(".Card__Header__Title");
+        matchInfo.league = leagueElement ? leagueElement.textContent.trim() : '';
+    
+        const homeTeamElement = container.querySelector(".ScoreboardScoreCell__Item--home .ScoreCell__TeamName");
+        matchInfo.homeTeam = homeTeamElement ? homeTeamElement.textContent.trim() : '';
+    
+        const awayTeamElement = container.querySelector(".ScoreboardScoreCell__Item--away .ScoreCell__TeamName");
+        matchInfo.awayTeam = awayTeamElement ? awayTeamElement.textContent.trim() : '';
+    
+        const homeScoreElement = container.querySelector(".ScoreboardScoreCell__Item--home .ScoreCell__Score");
+        matchInfo.homeScore = homeScoreElement ? homeScoreElement.textContent.trim() : '';
+    
+        const awayScoreElement = container.querySelector(".ScoreboardScoreCell__Item--away .ScoreCell__Score");
+        matchInfo.awayScore = awayScoreElement ? awayScoreElement.textContent.trim() : '';
+    
+        const matchStatusElement = container.querySelector(".ScoreCell__Time");
+        matchInfo.matchStatus = matchStatusElement ? matchStatusElement.textContent.trim() : '';
+    
+        const homeLogoElement = container.querySelector(".ScoreboardScoreCell__Item--home .ScoreboardScoreCell__Logo");
+        matchInfo.homeLogo = homeLogoElement ? homeLogoElement.getAttribute("src") : '';
+    
+        const awayLogoElement = container.querySelector(".ScoreboardScoreCell__Item--away .ScoreboardScoreCell__Logo");
+        matchInfo.awayLogo = awayLogoElement ? awayLogoElement.getAttribute("src") : '';
+    
+        matchData.push(matchInfo);
     });
+
+      return matchData;
+    });
+
+    await browser.close();
 
     res.json({ matches: matchData });
-
   } catch (error) {
     console.error("Error scraping:", error);
     res.status(500).json({ error: "Error scraping data" });
