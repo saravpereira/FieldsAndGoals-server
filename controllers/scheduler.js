367dbe1f9a54f605020c4e164e11649dfc20793d
Nathan Pham <87776690+nghiapham1026@users.noreply.github.com>
2023-08-31 22:37:15 +0000

    Minor edit to scheduler.js

--- controllers/scheduler.js
+++ controllers/scheduler.js
@@ -1,11 +1,18 @@
 const cron = require("node-cron");
 const { scheduleCall } = require("./webscrapeController.js");
+const fs = require("fs");
 
 cron.schedule("0 2 * * *", async () => {
   try {
+    const logMessage = `Scheduled task started at ${new Date()}\n`;
+    fs.appendFileSync("scheduler_log.txt", logMessage);
+
     console.log("Scheduled task started");
     await scheduleCall();
     console.log("Scheduled task completed");
+
+    const completionLogMessage = `Scheduled task completed at ${new Date()}\n`;
+    fs.appendFileSync("scheduler_log.txt", completionLogMessage);
   } catch (error) {
     console.error("Scheduled task error:", error);
   }
