7c86109a0f985fead89563968607aae0ccb0f47f
Nathan Pham <nghiapham102603@gmail.com>
2023-09-02 10:37:36 -0700

    Fixed "Error scraping: TimeoutError"

--- models/dates.js
+++ models/dates.js
@@ -1,6 +1,14 @@
 function getDateRange() {
-  const startDate = "20230831";
-  const endDate = "20230901";
+  const startYear = "2023";
+  const endYear = "2023";
+  const startMonth = "8";
+  const endMonth = "8";
+  const startDay = "27";
+  const endDay = "31";
+
+  // Combine the date components into formatted strings
+  const startDate = `${startYear}${startMonth.padStart(2, "0")}${startDay.padStart(2, "0")}`;
+  const endDate = `${endYear}${endMonth.padStart(2, "0")}${endDay.padStart(2, "0")}`;
 
   return {
     startDate,
