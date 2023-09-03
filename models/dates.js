8a8760d725c7ef02cf03e5584028cfe905c78e4e
Nathan Pham <nghiapham102603@gmail.com>
2023-09-02 17:15:39 -0700

    Specified collection for POST

--- models/dates.js
+++ models/dates.js
@@ -6,7 +6,6 @@ function getDateRange() {
   const startDay = "27";
   const endDay = "31";
 
-  // Combine the date components into formatted strings
   const startDate = `${startYear}${startMonth.padStart(2, "0")}${startDay.padStart(2, "0")}`;
   const endDate = `${endYear}${endMonth.padStart(2, "0")}${endDay.padStart(2, "0")}`;
 
--- models/todo.js
+++ models/todo.js
@@ -1,46 +1,49 @@
 const mongoose = require("mongoose");
 
-const todoSchema = mongoose.Schema({
-  matches: [
-    {
-      matchDate: {
-        type: String,
-        required: true,
+const todoSchema = mongoose.Schema(
+  {
+    matches: [
+      {
+        matchDate: {
+          type: String,
+          required: true,
+        },
+        league: {
+          type: String,
+          required: true,
+        },
+        homeTeam: {
+          type: String,
+          required: true,
+        },
+        awayTeam: {
+          type: String,
+          required: true,
+        },
+        homeScore: {
+          type: String,
+          required: true,
+        },
+        awayScore: {
+          type: String,
+          required: true,
+        },
+        matchStatus: {
+          type: String,
+          required: true,
+        },
+        homeLogo: {
+          type: String,
+          required: true,
+        },
+        awayLogo: {
+          type: String,
+          required: true,
+        },
       },
-      league: {
-        type: String,
-        required: true,
-      },
-      homeTeam: {
-        type: String,
-        required: true,
-      },
-      awayTeam: {
-        type: String,
-        required: true,
-      },
-      homeScore: {
-        type: String,
-        required: true,
-      },
-      awayScore: {
-        type: String,
-        required: true,
-      },
-      matchStatus: {
-        type: String,
-        required: true,
-      },
-      homeLogo: {
-        type: String,
-        required: true,
-      },
-      awayLogo: {
-        type: String,
-        required: true,
-      },
-    }
-  ],
-});
+    ],
+  },
+  { collection: "sampleMatchData"}
+);
 
 module.exports = mongoose.model("Todo", todoSchema);
