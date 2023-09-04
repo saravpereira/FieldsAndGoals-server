13d280348d5883e200944059ae3b1cf80f374b29
Nathan Pham <87776690+nghiapham1026@users.noreply.github.com>
2023-09-04 06:06:25 +0000

    Shortened webscrapeController.js

--- controllers/webscrapeController.js
+++ controllers/webscrapeController.js
@@ -1,96 +1,46 @@
 const puppeteer = require("puppeteer");
 const { scoresURL } = require("./constants");
 
-async function scrapeEspn(startDate, endDate, req, res) {
+const scrapeEspn = async (startDate, endDate, req, res) => {
   try {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-
     await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 7_4_0; like Mac OS X) AppleWebKit/534.49 (KHTML, like Gecko)  Chrome/53.0.1780.199 Mobile Safari/533.3');
-
     const allMatchData = [];
 
     for (let currentDate = startDate; currentDate <= endDate; currentDate++) {
-      const url = `${scoresURL}/_/date/${currentDate}`;
-      
-      await page.goto(url);
+      await page.goto(`${scoresURL}/_/date/${currentDate}`);
       await page.waitForSelector(".Scoreboard__RowContainer", { timeout: 120000 });
 
-      const matchData = await page.evaluate((date) => {
-        const matchData = [];
-      
-        const matchElements = document.querySelectorAll(".Scoreboard__RowContainer");
-      
-        matchElements.forEach((container) => {
-          const matchInfo = {};
-      
-          const dateString = date.toString();
-          const matchDateObj = new Date(
-            parseInt(dateString.substring(0, 4)),
-            parseInt(dateString.substring(4, 6)) - 1,
-            parseInt(dateString.substring(6, 8))
-          );
-          const options = { year: 'numeric', month: 'long', day: 'numeric' };
-          matchInfo.matchDate = matchDateObj.toLocaleDateString('en-US', options);
-          
-          const leagueElement = container.closest(".Card")?.querySelector(".Card__Header__Title");
-          matchInfo.league = leagueElement ? leagueElement.textContent.trim() : '';
-    
-          const homeTeamElement = container.querySelector(".ScoreboardScoreCell__Item--home .ScoreCell__TeamName");
-          matchInfo.homeTeam = homeTeamElement ? homeTeamElement.textContent.trim() : '';
-    
-          const awayTeamElement = container.querySelector(".ScoreboardScoreCell__Item--away .ScoreCell__TeamName");
-          matchInfo.awayTeam = awayTeamElement ? awayTeamElement.textContent.trim() : '';
-    
-          const homeScoreElement = container.querySelector(".ScoreboardScoreCell__Item--home .ScoreCell__Score");
-          matchInfo.homeScore = homeScoreElement ? homeScoreElement.textContent.trim() : '';
-    
-          const awayScoreElement = container.querySelector(".ScoreboardScoreCell__Item--away .ScoreCell__Score");
-          matchInfo.awayScore = awayScoreElement ? awayScoreElement.textContent.trim() : '';
-    
-          const matchStatusElement = container.querySelector(".ScoreCell__Time");
-          matchInfo.matchStatus = matchStatusElement ? matchStatusElement.textContent.trim() : '';
-    
-          const homeLogoElement = container.querySelector(".ScoreboardScoreCell__Item--home .ScoreboardScoreCell__Logo");
-          matchInfo.homeLogo = homeLogoElement ? homeLogoElement.getAttribute("src") : '';
-    
-          const awayLogoElement = container.querySelector(".ScoreboardScoreCell__Item--away .ScoreboardScoreCell__Logo");
-          matchInfo.awayLogo = awayLogoElement ? awayLogoElement.getAttribute("src") : '';
-
-          if (matchInfo.homeScore === '') {
-            matchInfo.homeScore = ' ';
-          }
-        
-          if (matchInfo.awayScore === '') {
-            matchInfo.awayScore = ' ';
-          }
-
-          if (!matchInfo.homeLogo) {
-            matchInfo.homeLogo = 'https://www.seekpng.com/png/full/28-289657_espn-soccer-team-logo-default.png';
-          }
-        
-          if (!matchInfo.awayLogo) {
-            matchInfo.awayLogo = 'https://www.seekpng.com/png/full/28-28957_espn-soccer-team-logo-default.png';
-          }
-          
-          matchData.push(matchInfo);
+      const matchData = await page.evaluate(date => {
+        date = date.toString();
+        return [...document.querySelectorAll(".Scoreboard__RowContainer")].map(container => {
+          const getText = selector => container.querySelector(selector)?.textContent.trim() || '';
+          const getAttr = (selector, attr) => container.querySelector(selector)?.getAttribute(attr) || '';
+          const dateObj = new Date(parseInt(date.substring(0, 4)), parseInt(date.substring(4, 6)) - 1, parseInt(date.substring(6, 8)));
+          return {
+            matchDate: dateObj.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }),
+            league: getText(".Card__Header__Title"),
+            homeTeam: getText(".ScoreboardScoreCell__Item--home .ScoreCell__TeamName"),
+            awayTeam: getText(".ScoreboardScoreCell__Item--away .ScoreCell__TeamName"),
+            homeScore: getText(".ScoreboardScoreCell__Item--home .ScoreCell__Score") || ' ',
+            awayScore: getText(".ScoreboardScoreCell__Item--away .ScoreCell__Score") || ' ',
+            matchStatus: getText(".ScoreCell__Time"),
+            homeLogo: getAttr(".ScoreboardScoreCell__Item--home .ScoreboardScoreCell__Logo", "src") || 'https://www.seekpng.com/png/full/28-289657_espn-soccer-team-logo-default.png',
+            awayLogo: getAttr(".ScoreboardScoreCell__Item--away .ScoreboardScoreCell__Logo", "src") || 'https://www.seekpng.com/png/full/28-28957_espn-soccer-team-logo-default.png'
+          };
         });
-
-        return matchData;
-      }, currentDate);
+      }, currentDate.toString());;
 
       allMatchData.push(...matchData);
     }
 
     await browser.close();
-
     res.json({ matches: allMatchData });
   } catch (error) {
     console.error("Error scraping:", error);
     res.status(500).json({ error: "Error scraping data" });
   }
-}
-
-module.exports = {
-  scrapeEspn
 };
+
+module.exports = { scrapeEspn };
