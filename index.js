8771a6ff0b41da544fc4d5d10ed51f61930708ec
Nathan Pham <nghiapham102603@gmail.com>
2023-09-16 20:44:08 -0700

    Scheduling job with cron

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cron = require("node-cron");
 require("dotenv").config();
 
 const matchRoutes = require('./routes/match');
+const matchController = require('./controllers/match');
 
 async function main() {
   await mongoose.connect(process.env.DB);
@@ -15,6 +17,16 @@ main()
 const app = express();
 
 app.use('/espn', matchRoutes);
+
+cron.schedule('* * * * *', async () => {
+  console.log('Cron job started at:', new Date());
+  try {
+    await matchController.getPastResults();
+    console.log('Cron job completed successfully at:', new Date());
+  } catch (error) {
+    console.log('Cron job failed with error:', error);
+  }
+});
 
 app.listen(8080, () => {
   console.log("Server listening on port 8080");
