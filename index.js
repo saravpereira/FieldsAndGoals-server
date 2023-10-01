04fdff419ae4d99fca0eefb6502688e33be30f2b
Sara Pereira <sarapereira0420@gmail.com>
2023-10-01 13:22:16 -0700

    comment

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const cron = require("node-cron");
 require("dotenv").config();
 
-const matchRoutes = require('./routes/match');
-const matchController = require('./controllers/match');
+const matchRoutes = require("./routes/match");
+const matchController = require("./controllers/match");
 
 async function main() {
   await mongoose.connect(process.env.DB);
@@ -16,17 +16,17 @@ main()
 
 const app = express();
 
-app.use('/espn', matchRoutes);
+app.use("/espn", matchRoutes);
 
-cron.schedule('0 0 * * *', async () => { //Execute at 00:00 daily
-  console.log('Cron job started at:', new Date());
-  try {
-    await matchController.getPastResults();
-    console.log('Cron job completed successfully at:', new Date());
-  } catch (error) {
-    console.log('Cron job failed with error:', error);
-  }
-});
+// cron.schedule('0 0 * * *', async () => { //Execute at 00:00 daily
+//   console.log('Cron job started at:', new Date());
+//   try {
+//     await matchController.getPastResults();
+//     console.log('Cron job completed successfully at:', new Date());
+//   } catch (error) {
+//     console.log('Cron job failed with error:', error);
+//   }
+// });
 
 app.listen(8080, () => {
   console.log("Server listening on port 8080");
