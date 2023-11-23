19077dd313103b59bf76e1c4a96552aed78e28e9
Nathan Pham <87776690+nghiapham1026@users.noreply.github.com>
2023-11-23 10:53:13 -0800

    BE: server-side cache for getGamesByDate (#19)
    * Added cache for getGamesByDate
    
    * Linting
    
    * Changed cache lifetime to 30 days
    
    * Implemented database caching
    
    * Node-cache

---
+++ utils/cache.js
@@ -0,0 +1,33 @@
+const NodeCache = require('node-cache');
+const CACHE_LIFETIME = 30 * 24 * 60 * 60;
+
+const myCache = new NodeCache({ stdTTL: CACHE_LIFETIME, checkperiod: CACHE_LIFETIME * 0.2 });
+
+const setCache = (key, data) => {
+  myCache.set(key, data);
+};
+
+const getCache = (key) => {
+  const data = myCache.get(key);
+  if (data) {
+    return {
+      data: data,
+      isCached: true,
+    };
+  }
+  return null;
+};
+
+const clearCache = (key = null) => {
+  if (key) {
+    myCache.del(key);
+  } else {
+    myCache.flushAll();
+  }
+};
+
+module.exports = {
+  setCache,
+  getCache,
+  clearCache,
+};
