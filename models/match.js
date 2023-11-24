11cff0c4abb00d1f36748de45c036f748df31934
Nathan Pham <87776690+nghiapham1026@users.noreply.github.com>
2023-11-24 03:55:26 +0000

    BE: Each match needs to be a separate entry in the db

--- models/match.js
+++ models/match.js
@@ -2,46 +2,42 @@ const mongoose = require('mongoose');
 
 const matchSchema = mongoose.Schema(
   {
-    matches: [
-      {
-        matchDate: {
-          type: String,
-          required: true,
-        },
-        league: {
-          type: String,
-          required: true,
-        },
-        homeTeam: {
-          type: String,
-          required: true,
-        },
-        awayTeam: {
-          type: String,
-          required: true,
-        },
-        homeScore: {
-          type: String,
-          required: true,
-        },
-        awayScore: {
-          type: String,
-          required: true,
-        },
-        matchStatus: {
-          type: String,
-          required: true,
-        },
-        homeLogo: {
-          type: String,
-          required: true,
-        },
-        awayLogo: {
-          type: String,
-          required: true,
-        },
-      },
-    ],
+    matchDate: {
+      type: String,
+      required: true,
+    },
+    league: {
+      type: String,
+      required: true,
+    },
+    homeTeam: {
+      type: String,
+      required: true,
+    },
+    awayTeam: {
+      type: String,
+      required: true,
+    },
+    homeScore: {
+      type: String,
+      required: true,
+    },
+    awayScore: {
+      type: String,
+      required: true,
+    },
+    matchStatus: {
+      type: String,
+      required: true,
+    },
+    homeLogo: {
+      type: String,
+      required: true,
+    },
+    awayLogo: {
+      type: String,
+      required: true,
+    },
   },
   { collection: 'matchData' }
 );
